f2f7256d74ea2d3f39d486362a7afce9d398c9b3
Spencer <spencer.woo@liferay.com>
2017-11-03 11:41:10 -0700

    Improve output

--- src/true-blame.py
+++ src/true-blame.py
@@ -7,19 +7,20 @@ dir_path = os.getcwd()
 def run_blame(fn, ln):
     line_range = ln + "," + ln
 
-    process = subprocess.Popen(["git", "blame", fn, "-L", line_range], stdout=subprocess.PIPE, cwd=dir_path)
+    process = subprocess.Popen(["git", "blame", fn, "-L", line_range, "-p"], stdout=subprocess.PIPE, cwd=dir_path)
 
-    return str(process.communicate()[0])
+    return (process.communicate()[0]).decode("UTF-8")
 
 if (len(sys.argv) < 3):
-    print("Filename: ", flush=True)
+    print("Filename: ", end="", flush=True)
     file_name = input()
-    print("Starting line: ", flush=True)
+    print("Starting line: ", end="", flush=True)
     line_number = input()
 else :
     file_name = sys.argv[1]
     line_number = sys.argv[2]
 
 print(dir_path)
+print()
 
 print(run_blame(file_name, line_number))
