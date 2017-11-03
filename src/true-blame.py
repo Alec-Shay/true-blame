4b2ad88111abe51781668592b2cba57b38577d0b
Spencer <spencer.woo@liferay.com>
2017-11-03 11:57:00 -0700

    git log the git blame hash

--- src/true-blame.py
+++ src/true-blame.py
@@ -4,12 +4,18 @@ import sys
 
 dir_path = os.getcwd()
 
+def run_log(blame_hash):
+	process = subprocess.Popen(["git","log", blame_hash, "-1", "-p", "--full-diff"], stdout=subprocess.PIPE)
+	git_log = (process.communicate()[0]).decode("UTF-8")
+
+	return git_log
+
 def run_blame(fn, ln):
     line_range = ln + "," + ln
 
     process = subprocess.Popen(["git", "blame", fn, "-L", line_range, "-p"], stdout=subprocess.PIPE, cwd=dir_path)
-
-    return (process.communicate()[0]).decode("UTF-8")
+    git_blame = (process.communicate()[0]).decode("UTF-8")
+    return git_blame
 
 if (len(sys.argv) < 3):
     print("Filename: ", end="", flush=True)
@@ -20,7 +26,8 @@ else :
     file_name = sys.argv[1]
     line_number = sys.argv[2]
 
-print(dir_path)
-print()
+git_blame = run_blame(file_name, line_number)
+blame_hash = git_blame.split()[0]
 
-print(run_blame(file_name, line_number))
+git_log = run_log(blame_hash)
+print(git_log)
