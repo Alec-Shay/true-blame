cc31f0c3443ca5c10a9e97589055a1efeb6d0010
Alec Shay <Alec.Shay@liferay.com>
2017-11-03 11:05:18 -0700

    Perform git blame

---
+++ src/true-blame.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+import sys
+
+dir_path = os.getcwd()
+
+if (len(sys.argv) < 3):
+    print("Filename: ", flush=True)
+    file_name = input()
+    print("Starting line: ", flush=True)
+    line_number = input()
+else :
+    file_name = sys.argv[1]
+    line_number = sys.argv[2]
+
+print(dir_path)
+
+process = subprocess.Popen(["git", "blame", file_name, "-L", line_number + "," + line_number, "-p"], stdout=subprocess.PIPE, cwd=dir_path)
+
+print(str(process.communicate()[0]))
