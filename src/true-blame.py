f70354b9f36c7a506acf9224a101d72c933de7ac
Alec Shay <Alec.Shay@liferay.com>
2017-11-03 17:21:41 -0700

    Fixing things step 1

--- src/true-blame.py
+++ src/true-blame.py
@@ -31,14 +31,17 @@ def run_diff(blame_hash):
         return gitDiff
 
 def run_blame(fn, ln, head):
-    line_range = ln + "," + ln
+	line_range = str(ln) + "," + str(ln)
 
-    process = subprocess.Popen(["git", "blame", fn, "-L", line_range, head, "-p"], stdout=subprocess.PIPE, cwd=dir_path)
-    git_blame = (process.communicate()[0]).decode("UTF-8")
-    return git_blame
+	print("git blame " + fn + " " + "-L" + " " + line_range + " " + head + "-p")
+
+	process = subprocess.Popen(["git", "blame", fn, "-L", line_range, head, "-p"], stdout=subprocess.PIPE, cwd=dir_path)
+	git_blame = (process.communicate()[0]).decode("UTF-8")
+	return git_blame
 
 def get_file_diffs(git_log):
 	file_diffs = { }
+	separate_diffs_list = [ ]
 	split_log = git_log.split("diff --git a/")
 	split_log.pop(0)
 
@@ -47,17 +50,22 @@ def get_file_diffs(git_log):
 		fileName = fileName.split("/")[-1]
 
 		fileDiff = log.split("@@")
+
 		if len(fileDiff) < 2:
 			print("ERROR: Invalid Diff Target")
 			break
-		fileDiff = "@@" + fileDiff[1] + "@@" + fileDiff[2]
 
-		file_diffs[fileName] = fileDiff
+		for i in range(int(len(fileDiff) / 2)):
+			separate_diffs_list.append("@@" + fileDiff[i + 1] + "@@" + fileDiff[i + 2])
+
+		file_diffs[fileName] = separate_diffs_list
 
 	return file_diffs
 
 def recursive_blame(file_name, line_number, substring, head):
-	while True:
+	blaming = True
+
+	while blaming:
 		git_blame = run_blame(file_name, line_number, head)
 		try:
 			blame_hash = git_blame.split()[0]
@@ -66,23 +74,60 @@ def recursive_blame(file_name, line_number, substring, head):
 			sys.exit(0)
 
 		git_diff_result = run_diff(blame_hash)
-		file_diffs = get_file_diffs(git_diff_result)
+		file_diffs_map = get_file_diffs(git_diff_result)
 
-		# for x in file_diffs:
-		# 	print(file_diffs.get(x))
+		#print("git_diff_result: " + git_diff_result)
 
 		if "/" in file_name:
 			file_name2 = file_name.split("/")[-1]
 
-		content = file_diffs.get(file_name2)
+		content_list = file_diffs_map.get(file_name2)
 
-		for line in content.split("\n"):
-			if line:
-				if line[0] is "-":
-					if line.find(substring) > -1:
-						head = blame_hash + "^"
-						continue
-		return blame_hash
+		for c in content_list:
+			print(c)
+
+		for content in content_list:
+			content_lines = content.split("\n")
+
+			diff_line_tokens = content_lines[0].split(" ")
+
+			base_line_number = -1
+
+			for token in diff_line_tokens:
+				if token[0] is "-":
+					if token.find(",") > -1:
+						base_line_number = token.split(",")[0]
+					else:
+						base_line_number = token
+
+					base_line_number = base_line_number.replace("-", "")
+
+					if base_line_number == "226":
+						print("token: " + token)
+					break
+
+			blaming = False
+
+			for i, line in enumerate(content_lines):
+				if line:
+					if line[0] is "-":
+						if line.find(substring) > -1:
+							line_number = str(int(base_line_number) + i + 1)
+
+							head = blame_hash
+
+							print("content: " + content)
+							print(type(base_line_number))
+							print("base_line_number: " + str(base_line_number))
+							print("line number: " + line_number)
+							print("head: " + head)
+							blaming = True
+							break
+
+			if blaming:
+				break
+
+	return blame_hash
 
 def main():
 	if (len(sys.argv) < 3):
@@ -94,13 +139,17 @@ def main():
 	    substring = input()
 
 	    # FOR TESTING
-	    # file_name = "modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/java/com/liferay/dynamic/data/mapping/type/text/internal/TextDDMFormFieldTypeSettings.java"
-	    # line_number = "98"
-	    # substring = "\"allowEmptyOptions=true\""
+	    #file_name = "modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/java/com/liferay/dynamic/data/mapping/type/text/internal/TextDDMFormFieldTypeSettings.java"
+	    #line_number = "98"
+	    #substring = "\"allowEmptyOptions=true\""
 
-	    file_name = "portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java"
-	    line_number = "209"
-	    substring = "sb.append(StringPool.SPACE)"
+	    #file_name = "portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java"
+	    #line_number = "209"
+	    #substring = "sb.append(StringPool.SPACE)"
+
+	    file_name = "modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java"
+	    line_number = "157"
+	    substring = "rootPortletId"
 	else:
 	    file_name = sys.argv[1]
 	    line_number = sys.argv[2]
@@ -112,7 +161,7 @@ def main():
 	                substring = sys.argv[i + 1]
 	                break
 	head = "HEAD"
-	print(get_line(file_name, line_number))
+	#print(get_line(file_name, line_number))
 
 	blame_hash = recursive_blame(file_name, line_number, substring, head)
 	print(blame_hash)
