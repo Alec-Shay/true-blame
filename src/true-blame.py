42142e9408805ffebbaae8bcb0b1601518777fde
Alec Shay <Alec.Shay@liferay.com>
2017-11-07 12:12:36 -0800

    Make reverse work

--- src/true-blame.py
+++ src/true-blame.py
@@ -4,14 +4,19 @@ import subprocess
 import sys
 
 dir_path = os.getcwd()
-
+reverse = False
+reverse_end_point = "HEAD"
 
 # GIT
 def git_blame(file_name, line_number, head):
-    line_range = str(line_number) + "," + str(line_number)
+	line_range = str(line_number) + "," + str(line_number)
 
-    args = ["-L", line_range, "-p", head, "--", file_name]
-    return git_process("blame", args)
+	if reverse:
+		args = ["-L", line_range, "-p", "--reverse", head, "--", file_name]
+	else:
+		args = ["-L", line_range, "-p", head, "--", file_name]
+
+	return git_process("blame", args)
 
 
 def git_diff(blame_hash, parent_hash):
@@ -19,10 +24,13 @@ def git_diff(blame_hash, parent_hash):
     return git_process("diff", args)
 
 
+def git_rev_list_with_ancestry_path(commit_hash):
+	args = ["--ancestry-path", commit_hash + ".." + reverse_end_point]
+	return git_process("rev-list", args)
+
 def git_rev_parse(hash):
     args = [hash + "^"]
     return git_process("rev-parse", args).replace("\n","")
-
 
 def git_process(cmd, *params):
     print("git " + cmd + " " + ' '.join(str(x) for x in params[0]))
@@ -34,6 +42,16 @@ def git_process(cmd, *params):
 
 
 # UTIL
+def fix_current_blame_with_hash(blame, hash):
+	blame_lines = blame.splitlines()
+
+	new_blame = hash + blame_lines[0][blame_lines[0].find(" "):]
+
+	for i, x in enumerate(blame_lines):
+		new_blame += "\n" + x
+
+	return new_blame
+
 def get_line(file_name, line_number):
     file = open(dir_path + "/" + file_name)
     line = ""
@@ -73,6 +91,17 @@ def get_file_diffs(git_log):
     return file_diffs
 
 
+def get_blame_parent(blame_hash, blame):
+	if reverse:
+		parent_hash = git_rev_parse(blame_hash)
+	else:
+		regex_string = "((previous)((.)*)(\.)([a-zA-Z]+)((\s)*)(filename))"
+		other = re.compile(regex_string).split(blame)[1]
+		parent_hash = other.split()[1]
+
+	return parent_hash
+
+
 def sort_file_diffs(diffs, file_name):
     sorted_diffs = {}
     sorted_diffs[file_name] = "temp"
@@ -100,6 +129,11 @@ def parse_diffs(input_params, sorted_diffs):
     blame_hash = input_params['blame_hash']
     substring = input_params['substring']
 
+    if reverse:
+    	relevant_char = "+"
+    else:
+    	relevant_char = "-"
+
     for diff_file_name, content_lines in sorted_diffs.items():
         for content in content_lines:
             base_line_number = -1
@@ -107,24 +141,25 @@ def parse_diffs(input_params, sorted_diffs):
 
             diff_line_tokens = content_lines[0].split(" ")
             for token in diff_line_tokens:
-                if token[0] is "-":
+                if token[0] is relevant_char:
                     if token.find(",") > -1:
                         base_line_number = token.split(",")[0]
                     else:
                         base_line_number = token
 
-                    base_line_number = base_line_number.replace("-", "")
+                    base_line_number = base_line_number.replace(relevant_char, "")
                     break
 
-            removal_lines = -1
+            relevant_lines = -1
+
             for i, line in enumerate(content_lines):
                 if line:
-                    if line[0] is "-":
-                        removal_lines += 1
+                    if line[0] is relevant_char:
+                        relevant_lines += 1
 
                         if line.find(substring) > -1:
                             print("Traced to: " + blame_hash)
-                            line_number = int(base_line_number) + removal_lines
+                            line_number = int(base_line_number) + relevant_lines
 
                             return_params['head'] = blame_hash + "^"
                             return_params['file_name'] = diff_file_name
@@ -137,103 +172,144 @@ def parse_diffs(input_params, sorted_diffs):
 
 
 def recursive_blame(file_name, line_number, substring, head):
-    blaming = True
+	blaming = True
 
-    while blaming:
-        print("==============")
-        print("Checking : " + head)
-        current_blame = git_blame(file_name, line_number, head)
-        try:
-            blame_hash = current_blame.split()[0]
-            # Refactor later (regex)
-            regex_string = "((previous)((.)*)(\.)([a-zA-Z]+)((\s)*)(filename))"
-            other = re.compile(regex_string).split(current_blame)[1]
-            parent_hash = other.split()[1]
-        except:
-            return blame_hash
+	while blaming:
+		print("==============")
+		print("Checking : " + head)
+		current_blame = git_blame(file_name, line_number, head)
 
-        git_diff_result = git_diff(blame_hash, parent_hash)
-        file_diffs_map = get_file_diffs(git_diff_result)
+		try:
+			blame_hash = current_blame.split()[0]
 
-        sorted_diffs = sort_file_diffs(file_diffs_map, file_name)
+			if reverse:
+				parent_hash = blame_hash
 
-        input_params = {'blame_hash': blame_hash,
-                        'substring': substring}
-        output_params = parse_diffs(input_params, sorted_diffs)
+				ancestry = git_rev_list_with_ancestry_path(blame_hash).splitlines()
 
-        blaming = output_params['blaming']
-        if blaming:
-            head = output_params['head']
-            file_name = output_params['file_name']
-            line_number = output_params['line_number']
-        # for k, v in output_params:
-        #     locals()['k'] = v
+				try:
+					blame_hash = ancestry[-1]
+				except:
+					return current_blame
+			else:
+				try:
+					parent_hash = get_blame_parent(blame_hash, current_blame)
+				except:
+					return current_blame
+		except:
+			print("ERROR: Invalid commit for git blame.")
+			sys.exit(0)
 
-    return blame_hash
+		git_diff_result = git_diff(blame_hash, parent_hash)
+		file_diffs_map = get_file_diffs(git_diff_result)
+
+		sorted_diffs = sort_file_diffs(file_diffs_map, file_name)
+
+		input_params = {'blame_hash': blame_hash,
+						'substring': substring}
+		output_params = parse_diffs(input_params, sorted_diffs)
+
+		blaming = output_params['blaming']
+		if blaming:
+			head = output_params['head']
+			file_name = output_params['file_name']
+			line_number = output_params['line_number']
+		# for k, v in output_params:
+		#     locals()['k'] = v
+
+	if reverse:
+		current_blame = fix_current_blame_with_hash(current_blame, blame_hash)
+
+	return current_blame
 
 
 def main():
-    substring = None
+	substring = None
+	global reverse
+	global reverse_end_point
 
-    if (len(sys.argv) < 3):
-        print("Filename: ", end="", flush=True)
-        file_name = input()
-        print("Line number: ", end="", flush=True)
-        line_number = input()
-        print("Substring (enter nothing to trace exact line): ", flush=True)
-        substring = input()
+	head = "HEAD"
 
-        # FOR TESTING
-        #file_name = "modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/java/com/liferay/dynamic/data/mapping/type/text/internal/TextDDMFormFieldTypeSettings.java"
-        #line_number = "125"
-        #substring = "\"allowEmptyOptions=true\""
+	if (len(sys.argv) < 3):
+		print("Filename: ", end="", flush=True)
+		file_name = input()
+		print("Line number: ", end="", flush=True)
+		line_number = input()
+		print("Substring (enter nothing to trace exact line): ", flush=True)
+		substring = input()
+		print("Reverse? (Y/N) ", end="", flush=True)
+		reverse_string = input()
+		reverse = reverse_string[0] is "Y" or reverse_string[0] is "y"
 
-        # file_name = "portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java"
-        # line_number = "209"
-        # substring = "sb.append(StringPool.SPACE)"
-        # EXPECT : b2590ecfa4b8d6cbefdb65c5cc7949a23e33155b
+		if reverse:
+			print("Starting point for reverse blame (enter nothing for HEAD): ", end="", flush=True)
+			head = input()
 
-        file_name = "modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java"
-        line_number = "157"
-        substring = "rootPortletId"
-        # EXPECT : 23b974bc9510a06d2a359301c1d12fab4aa61cc5
+			if head is None:
+				head = "HEAD"
 
-        #file_name = "modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java"
-        #line_number = "21"
-        #substring = "AssetEntry"
+			print("Ending point for reverse blame (enter nothing for HEAD): ", end="", flush=True)
+			reverse_end_point = input()
 
-        #file_name = "portal-impl/src/com/liferay/portal/util/PortalImpl.java"
-        #line_number = "317"
-        #substring = "class Portal"
-    else:
-        file_name = sys.argv[1]
-        line_number = sys.argv[2]
+			if reverse_end_point is None:
+				reverse_end_point = "HEAD"
 
-        for i, x in enumerate(sys.argv):
-            print(sys.argv[i])
+		# FOR TESTING
+		#file_name = "modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-text/src/main/java/com/liferay/dynamic/data/mapping/type/text/internal/TextDDMFormFieldTypeSettings.java"
+		#line_number = "125"
+		#substring = "\"allowEmptyOptions=true\""
 
-            if x == "-s" and len(sys.argv) > (i + 1):
-                substring = sys.argv[i + 1]
+		# file_name = "portal-kernel/src/com/liferay/portal/kernel/util/StringUtil.java"
+		# line_number = "209"
+		# substring = "sb.append(StringPool.SPACE)"
+		# EXPECT : b2590ecfa4b8d6cbefdb65c5cc7949a23e33155b
 
-                print(sys.argv[i + 1])
-                break
+		file_name = "modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java"
+		line_number = "157"
+		substring = "rootPortletId"
+		# EXPECT : 23b974bc9510a06d2a359301c1d12fab4aa61cc5
 
-    if file_name.find("\\") > -1:
-        file_name = file_name.replace("\\", "/")
+		#file_name = "modules/apps/web-experience/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java"
+		#line_number = "21"
+		#substring = "AssetEntry"
 
-    head = "HEAD"
+		#file_name = "portal-impl/src/com/liferay/portal/util/PortalImpl.java"
+		#line_number = "317"
+		#substring = "class Portal"
+	else:
+		file_name = sys.argv[1]
+		line_number = sys.argv[2]
 
-    if substring is None:
-        substring = get_line(file_name, line_number).strip()
-    elif substring.find("\n") > -1:
-        print("ERROR: more than one line selected for blame.")
-        print("Exiting True Blame.")
+		for i, x in enumerate(sys.argv):
+			print(sys.argv[i])
 
-        sys.exit(0)
+			if x == "-s" and len(sys.argv) > (i + 1):
+				substring = sys.argv[i + 1]
 
-    blame_hash = recursive_blame(file_name, line_number, substring, head)
-    print("==============")
-    print("True Blame Commit : " + blame_hash)
+
+			if x == "-r":
+				reverse = True
+
+				if len(sys.argv) > (i + 1) and sys.argv[i + 1][0] is not "-":
+					head = sys.argv[i + 1]
+
+				if len(sys.argv) > (i + 2) and sys.argv[i + 2][0] is not "-":
+					reverse_end_point = sys.argv[i + 2]
+
+	if file_name.find("\\") > -1:
+		file_name = file_name.replace("\\", "/")
+
+	if substring is None:
+		substring = get_line(file_name, line_number).strip()
+	elif substring.find("\n") > -1:
+		print("ERROR: more than one line selected for blame.")
+		print("Exiting True Blame.")
+
+		sys.exit(0)
+
+	blame = recursive_blame(file_name, line_number, substring, head)
+	print("==============")
+	print("True Blame Commit : " + blame)
 
 
 main()
